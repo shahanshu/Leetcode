7ba4e9485f89f12407b0dd4a1bbeeea0cc991153
Anshu Shah <infoanshu99@gmail.com>
2026-01-13 12:31:52 +0545

    DAY_3_LEETCODE

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -3,7 +3,7 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         if not nums:
             return 0 
-        i=0
+        i=0     #here we need a reference that should be compared to the rest of the values 
         for j in range(1,len(nums)):
             if nums[j]!=nums[i]: #check the new element to the previous ones 
                 i=i+1
