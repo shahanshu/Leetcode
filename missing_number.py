7fae0b687659870b88139a3825c3d266dd8ff6c6
Anshu Shah <infoanshu99@gmail.com>
2026-01-15 12:54:45 +0545

    DAY_5_LEETCODE

---
+++ missing_number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        seen=set(nums)
+        ans=set()
+        for i in range(1,len(nums)+1):
+            if i not in seen:
+                ans.add(i)
+        return list(ans)
