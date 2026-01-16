986dfae81afae7d87b33e366b955e2f83cb27ff0
Anshu Shah <infoanshu99@gmail.com>
2026-01-16 13:50:06 +0545

    DAY_6_LEETCODE

---
+++ count_uniquestring.py
@@ -0,0 +1,8 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        time=Counter(s)
+        for i,val in enumerate(s):
+            if time[val]==1:
+                
+                return i
+        return -1
--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-anshu='hellow'
-from collections import Counter
-c=Counter(anshu)
-print(c)
-print(c.items()) # this will return the dict items
-print(c.most_common())
+i='anshu'
+for x in i:
+    print(x[2])
